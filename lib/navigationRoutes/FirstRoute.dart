5440c1827a7e4071965ec0978103bb1983e7b0d3
sanjeetkumar <sanjeet677088@gmail.com>
2021-08-11 15:54:11 +0530

    Navigator Push and Pop

---
+++ lib/navigationRoutes/FirstRoute.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'SecondRoute.dart';
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
